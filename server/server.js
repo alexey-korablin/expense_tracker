43d639611fd3be483f52e7e19d75cbc9b5c84eae
Alexey Korablin <Alexey_Korablin@epam.com>
2020-03-04 16:29:07 +0300

    #07 The first implementation of prod mode

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const path = require('path');
 
 const transaction = require('./routes/transactions');
 const connectDB = require('./db');
@@ -11,6 +12,23 @@ dotenv.config({ path: './config.env' });
 const app = express();
 
 app.use(express.json());
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Production mode
+// TODO:
+// !!! The production mode does not work
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, '../client/build')));
+  app.use('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../client', 'build', 'index.html'),
+    ),
+  );
+}
 
 app.use('/api/v1/transactions', transaction);
 
