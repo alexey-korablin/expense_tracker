1f4e76adddff74f2d54535ff8e9797a95faa39bf
Alexey Korablin <Alexey_Korablin@epam.com>
2020-03-05 13:26:55 +0300

    #08 Fixes production mode bugs

--- server/controllers/transactions.js
+++ server/controllers/transactions.js
@@ -6,8 +6,6 @@ const Transaction = require('../models/Transaction');
 exports.getTransactions = async (req, res, next) => {
   try {
     const transactions = await Transaction.find();
-    console.log(transactions);
-    debugger;
     return res.status(200).json({
       success: true,
       count: transactions.length,
--- server/server.js
+++ server/server.js
@@ -9,30 +9,29 @@ const connectDB = require('./db');
 
 dotenv.config({ path: './config.env' });
 
+connectDB();
+
 const app = express();
 
 app.use(express.json());
 
+// Development mode
 // Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+app.use('/api/v1/transactions', transaction);
+
 // Production mode
-// TODO:
-// !!! The production mode does not work
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, '../client/build')));
-  app.use('*', (req, res) =>
+  app.use(express.static('../client/build'));
+  app.get('*', (req, res) =>
     res.sendFile(
       path.resolve(__dirname, '../client', 'build', 'index.html'),
     ),
   );
 }
-
-app.use('/api/v1/transactions', transaction);
-
-connectDB();
 
 const PORT = process.env.PORT || 5000;
 
